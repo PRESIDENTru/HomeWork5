a8604d82fe8196659e51737baef3c84de9f0dd52
Danil <79894818+PRESIDENTru@users.noreply.github.com>
2023-02-15 00:30:40 +1000

    Delete HomeWork5 directory

--- HomeWork5/src/com/company/CreatePersonException.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class CreatePersonException extends Exception{
-
-    public CreatePersonException() {
-        super("Ошибка! Невозможно создать персонажа с таким именем!");
-    }
-}
